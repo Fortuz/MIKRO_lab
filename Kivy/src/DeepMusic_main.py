4eae0e53bbc91e01c088453cf90d0f7bb9245893
Balazs <fortuz19@gmail.com>
2020-03-04 13:20:31 +0100

    Global variables

--- Kivy/src/DeepMusic_main.py
+++ Kivy/src/DeepMusic_main.py
@@ -1,4 +1,5 @@
 import kivy
+import GlobalShared
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -10,51 +11,69 @@ from kivy.properties import StringProperty
 from kivy.uix.popup import Popup     
 from kivy.uix.floatlayout import FloatLayout
 
+class SmoothButton(Button):
+    btn_text_color = StringProperty(GlobalShared.BTN_TEXT_COLOR)
+
 # Popup Windows (Settings, Help, Credit)    
 class SettingsPopup(FloatLayout):
+    text_color = StringProperty(GlobalShared.TEXT_COLOR)
+    
     def btn_SettingsApply(self):
         print('SettingsApply')
+        GlobalShared.POPUP_WINDOW.dismiss()
+
 
 class HelpPopup(FloatLayout):
+    text_color = StringProperty(GlobalShared.TEXT_COLOR)
+    
     def btn_HelpBack(self):
         print('HelpBack')
+        GlobalShared.POPUP_WINDOW.dismiss()
 
 class CreditsPopup(FloatLayout):
+    text_color = StringProperty(GlobalShared.TEXT_COLOR)
+    
     def btn_CreditBack(self):
         print('CreditBack')
+        GlobalShared.POPUP_WINDOW.dismiss()
 
 
 # Main window class
 class MainWindow(Widget):
     image_src = StringProperty('pics/background_640x480.jpg')
+    background_color = StringProperty(GlobalShared.BACKGROUND_COLOR)
+    text_color = StringProperty(GlobalShared.TEXT_COLOR)
+    btn_text_color = StringProperty(GlobalShared.BTN_TEXT_COLOR)
     
     def btn_settings(self):
         print('Settings')
         show = SettingsPopup()
-        popupWindow = Popup(title="Settings", content=show, size_hint=(None, None), size=(400,400))
-        popupWindow.open()
+        GlobalShared.POPUP_WINDOW = Popup(title="Settings", title_color=GlobalShared.TEXT_COLOR, content=show, size_hint=(None, None), size=(400,400))
+        GlobalShared.POPUP_WINDOW.open()
         
     def btn_help(self):
         print('Help')
         show = HelpPopup()
-        popupWindow = Popup(title="Help", content=show, size_hint=(None, None), size=(400,400))
-        popupWindow.open()
+        GlobalShared.POPUP_WINDOW = Popup(title="Help", title_color=GlobalShared.TEXT_COLOR, content=show, size_hint=(None, None), size=(400,400))
+        GlobalShared.POPUP_WINDOW.open()
 
     def btn_credits(self):
         print('Credits')
         show = CreditsPopup()
-        popupWindow = Popup(title="Credits", content=show, size_hint=(None, None), size=(500,500))
-        popupWindow.open()
+        GlobalShared.POPUP_WINDOW = Popup(title="Credits", title_color=GlobalShared.TEXT_COLOR, content=show, size_hint=(None, None), size=(500,500))
+        GlobalShared.POPUP_WINDOW.open()
     
 
 # Main App class        
 class DeepMusic(App):
     def build(self):
         layout = MainWindow()
+        
         '''
-        Refresh displayed picture:
+        Refresh displayed picture example:
         # layout.image_src = 'pics/do.png'
         '''            
+        
         return layout
 
 
---
+++ Kivy/src/GlobalShared.py
@@ -0,0 +1,5 @@
+# Shared Global variables that all module can use
+POPUP_WINDOW = ''
+BACKGROUND_COLOR = (1, 1, 1, .2)
+TEXT_COLOR = (1, 1, 1, 1)
+BTN_TEXT_COLOR = (1, 1, 1, 1)
